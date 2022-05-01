1a75209668669052bcb505ddc01807073d925e7f
quyennguyen1985 <102743436+quyennguyen1985@users.noreply.github.com>
2022-05-01 10:28:00 +0700

    add e listener for C and F temp

--- weather.js
+++ weather.js
@@ -67,3 +67,9 @@ function searchCity(event) {
 
 let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", searchCity);
+let celciusTemp = null;
+search ("Ho Chi Minh city");
+let fLink = document.querySelector("#fdegree");
+fLink.addEventListener = ("click", displayFtemp);
+let cLink = document.querySelector("#cdegree");
+cLink.addEventListener = ("click", displayCtemp);
